9971ef7f5b8eab357ec7bd0105c69024bf4f80ce
stripthis <remy@passbolt.com>
2014-09-03 18:05:26 +0200

    menu and navigation

--- js/shipmap.js
+++ js/shipmap.js
@@ -3,7 +3,11 @@ var config = {
 	tileLayer : 'rbertot.ihpo2bcl',
 	featureLayers : {
 		'rbertot.ii696lei' : 'rw3',
-		'rbertot.jb84217i' : 'myez'
+		'rbertot.jb84217i' : 'myez',
+		'rbertot.jddm1ep5' : 'myas',
+		//'gpshipsmap.j3b5195d' : 'rw3',
+		//'gpshipsmap.j1jfcjmg' : 'myez',
+		//'gpshipsmap.j9bnjlbi' : 'myas'
 	},
 	mapOptions : {
 		center : [40, 0], 
@@ -23,21 +27,24 @@ var config = {
 			"iconSize": [60, 68],
 			"iconAnchor": [40, 48],
 			"popupAnchor": [0, -25],
-			"className": "ship"
+			"className": "ship",
+			"shipName" : 'rw3'
 		},
 		'e' : { // r = esperanza
 			"iconUrl": 'img/icon-espy.svg',
 			"iconSize": [60, 68],
 			"iconAnchor": [40, 48],
 			"popupAnchor": [0, -25],
-			"className": "ship"
+			"className": "ship",
+			"shipName" : 'myez'
 		},
 		'a' : { // a = arctic sunrise
 			"iconUrl": 'img/icon-as.svg',
 			"iconSize": [60, 68],
 			"iconAnchor": [40, 48],
 			"popupAnchor": [0, -25],
-			"className": "ship"
+			"className": "ship",
+			"shipName" : 'myas'
 		}
 	},
 	polyline : {
@@ -66,6 +73,9 @@ var app = {
 	minimap : undefined,
 	ships : {},
 	layers : {},
+	menu : {
+		'open' : true
+	},
 
 	// The clusterGroup gets each whitelisted marker in the group added to it
 	// once loaded, and then is added to the map
@@ -82,55 +92,64 @@ var app = {
 		app.layers[ship].addLayer(clusterGroup);
 	},
 
-	// Add a minimap
-	createMiniMap : function() {
-	 	//new L.Control.MiniMap(L.mapbox.tileLayer(config.tileLayer))
-		//	.addTo(app.map);
-		var minitile = new L.mapbox.tileLayer(config.tileLayer);
-		var miniMap = new L.Control.MiniMap(minitile, config.minimap);
-		miniMap.addTo(app.map);
-
+	// Add/Hide a minimap
+	toggleMiniMap : function() {
+		// add or remove layers
+		if (app.minimap !== undefined) {
+			$('.leaflet-control-minimap').toggleClass('hidden');
+		} else {
+			var minitile = new L.mapbox.tileLayer(config.tileLayer);
+			app.minimap = new L.Control.MiniMap(minitile, config.minimap)
+				.addTo(app.map);
+		}
 	},
 
 	// Add the line connecting the markers
-	connectTheDots : function(l, ship) {
+	// @param l featurelayer (some markers)
+	// @param s string ship name
+	connectTheDots : function(l, s) {
 		var line = [];
 		l.eachLayer(function(marker) {
 			line.push(marker.getLatLng());
 		});
-  		L.polyline(line, config.polyline).addTo(app.layers[ship]);
+  		L.polyline(line, config.polyline).addTo(app.layers[s]);
 	},
 
 	// Style markers with icon images based on their symbol 
-	styleMarkers : function(l) {
-		var marker = l,
-		    feature = marker.feature;
-		    
-		switch(feature.properties['marker-symbol']) {
-			case '':
-				marker.setIcon(L.icon(config.icons['']));
-			break;
-			case 'r':
-				marker.setIcon(L.icon(config.icons['r']));
-				app.ships['rw3'] = marker;
-			break;
-			case 'e':
-				marker.setIcon(L.icon(config.icons['e']));
-				app.ships['myez'] = marker;
-			break;
+	// @param marker layer
+	styleMarkers : function(marker) {
+		var symbol = marker.feature.properties['marker-symbol'];
+		var icon = config.icons[symbol];
+		// if the icon is specified in the app config
+		// we override the default one
+		if (icon !== undefined) {
+			marker.setIcon(L.icon(icon));
+			if (icon.className === 'ship' ) {
+				app.ships[icon.shipName] = marker;
+			}
 		}
 	},
 
+	// load copyright in leaflet reserved space
 	loadCopyright : function () {
 		$(".leaflet-control-attribution").html(config.copyright);
 	},
 
+	// bind UI navigation event to application logic
 	bindNavEvents : function () {
+		// show/hide menu tigger
+		$( ".menu-ctrl" ).click(function(e) {
+			e.preventDefault();
+			app.onMenuToggle();
+		});
+		// show/hidde ships buttons trigger
 		$( ".toggle" ).click(function(e) {
 			e.preventDefault();
 			s = this.getAttribute("data-ship");
-			app.onShipLayerToggle(s);
+			// do the real logic
+			app.onShipLayerToggle(s,this);
 		});
+		// goto ship button click trigger
 		$( ".pan" ).click(function(e) {
 			e.preventDefault();
 			s = this.getAttribute("data-ship");
@@ -138,7 +157,23 @@ var app = {
 		});
 	},
 
-	onShipLayerToggle : function(s) {
+	onMenuToggle : function(m) {
+		app.menu.open = !app.menu.open;
+		$('.menu-ctrl').toggleClass('open');
+		$('.menu-ctrl-target').toggleClass('hidden');
+		app.toggleMiniMap();
+	},
+
+	// what happen when clicking hide/show buttons for a given ship
+	// @param s string shipname
+	// @param a jquery object, the link that was clicked
+	onShipLayerToggle : function(s, a) {
+		// toggle button icon
+		$(a).children('i')
+			.toggleClass('fa-eye')
+			.toggleClass('fa-eye-slash');
+
+		// add or remove layers
 		if (app.map.hasLayer(app.layers[s])) {
 			app.map.removeLayer(app.layers[s]);
 		} else {
@@ -146,33 +181,38 @@ var app = {
 		}
 	},
 
+	// what happen when clicking the goto ship bottons
+	// @param s string shipname
 	onPanToShip : function(s) {
 		ship = app.ships[s];
         app.map.panTo(ship.getLatLng());
         ship.openPopup();
 	},
 
+	// what happen when a layer have been loaded via ajax
+	// @param l feature layer
 	onLayerReady : function(l) {
 		var ship = config.featureLayers[l._geojson.id];
 		app.createCluster(l, ship);
 		app.connectTheDots(l, ship);
 	},
 	
+	// what happen when all layers are loaded and ready
 	onAllLayerReady : function() {
 		app.bindNavEvents();
-		app.createMiniMap();
+		app.toggleMiniMap();
 		app.loadCopyright();
 	},
 	
+	// application entry point
 	init : function() {
 		// Here we avoid loading in markers from the layer. Instead we add just the
 		// backing tileLayer, and then use the featureLayers to add data on top of it.
 		app.map = L.map('map', config.mapOptions)
 			.addLayer(L.mapbox.tileLayer(config.tileLayer));
 
-		
 		// we don't directly use the feature layer used to load the markers
-		// instead we break it down into other layers and add it to app.layer
+		// instead we break it down into other smaller layers and add it to app.layer
 		var l,
 			i = 0,
 			imax = Object.keys(config.featureLayers).length;
@@ -192,15 +232,12 @@ var app = {
 						app.onAllLayerReady();
 					}
 				})
-				.on('click', function(e) {
+				.on('click', function(e) { // done for each click on markers
 		    		app.map.panTo(e.layer.getLatLng());
 	   			});
 		}
 	}
 }
-
+// haaaaaaaaardi matelot
 app.init();
-
-
-
 
